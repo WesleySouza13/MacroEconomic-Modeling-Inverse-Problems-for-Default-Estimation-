37c9e72ecd16a8f2cd6b85a00d6d91af88e5ca91
WesleySouza13 <souzabusiness13@gmail.com>
2026-07-26 15:24:20 -0300

    criando primeiros tramentos em EDA

--- dataEng/data_colect.py
+++ dataEng/data_colect.py
@@ -1,11 +1,11 @@
 from request_data import Request
 import pandas as pd 
 import os 
-lista_de_codes = [4393, 21084, 24369, 27640]
+lista_de_codes = [4393, 21084, 24369, 27640, 4390, 24364]
 datas = []
 for i in lista_de_codes:
     data_path = os.path.join('data', 'sgs_data', f'{i}.csv')
-    req = Request(i, '01/01/2010', '01/01/2026').req_data()
+    req = Request(i, '01/03/2012', '01/07/2026').req_data()
     req.to_csv(data_path)
     datas.append(req)
 df = pd.concat(datas, axis=1)
